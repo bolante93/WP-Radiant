6826776cc45f3633c4773d54b2c5d655a9c493c5
Jay Jules Bolante <bolante93@gmail.com>
2021-04-08 16:44:39 +0800

    Update home.php

--- home.php
+++ home.php
@@ -8,10 +8,17 @@ get_header();
 global $wp_query;
 ?>
     <div style="padding-top: 100px">
-        <div class="global-wrapper section-pading">
-        <?php while( have_posts() ): the_post(); ?>
-            <?php get_template_part('template-parts/content/content', 'home') ?>
-        <?php endwhile; ?>
+        <div class="global-wrapper section-padding">
+            <div class="posts active-sidebar">
+                <div class="post-wrapper">
+                    <?php while( have_posts() ): the_post(); ?>
+                        <?php get_template_part('template-parts/content/content', 'home') ?>
+                    <?php endwhile; ?>
+                </div>
+                <div class="sidebar">
+                    <?php get_sidebar() ?>
+                </div>
+            </div>
         </div>
         <?php classes\Components::pagination(); ?>
     </div>
